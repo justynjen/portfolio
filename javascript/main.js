46dd346f709d967c73715280fc27cff8eb96167c
justynjen <justyngourdin@gmail.com>
2017-07-31 08:51:11 -0600

    July.31
    Fixed some things on typed.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 //wait for document to load
 
 $('#typed').typed({
-  strings:["Web Developer.", "Blogger.", "Digital Nomad.", "Traveler."],
+  strings:["Web Developer.", "Blogger.", "Digital Nomad.","Ux/Ui Designer.", "Traveler."],
   typeSpeed: 90,
   backDelay: 800,
   loop: true,
-  cursor: true
+  cursor: false
 });
 });
