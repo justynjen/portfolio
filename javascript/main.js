b70626059f6917d4a782ef01952862078de2ef9c
justynjen <justyngourdin@gmail.com>
2017-07-31 02:59:37 -0600

    July.31
    added pictures and javascript to my website

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+//wait for document to load
+
+$('#typed').typed({
+  strings:["Web Developer.", "Blogger.", "Digital Nomad.", "Traveler."],
+  typeSpeed: 90,
+  backDelay: 800,
+  loop: true,
+  cursor: true
+});
+});
